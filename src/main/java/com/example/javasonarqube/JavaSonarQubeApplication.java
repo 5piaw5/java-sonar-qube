7979e379df410a0bdce6f8f772a0609cf7965c5d
Tian Suepwong <freshytech1@gmail.com>
2023-05-07 12:46:09 +0700

    update Project

---
+++ src/main/java/com/example/javasonarqube/JavaSonarQubeApplication.java
@@ -0,0 +1,13 @@
+package com.example.javasonarqube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSonarQubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSonarQubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javasonarqube/JavaSonarQubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javasonarqube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSonarQubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
